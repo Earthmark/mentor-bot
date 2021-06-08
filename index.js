cf8bd41d484c351f6b64a95ac4594d84b554882d
Daniel Miller (Earthmark) <earthmark.miller@gmail.com>
2021-06-07 17:29:03 -0700

    Added basic websocket based system

--- index.js
+++ index.js
@@ -2,37 +2,41 @@ import http from "http";
 import Discord from "discord.js";
 import Websocket from "ws";
 import dotenv from "dotenv";
+import qs from "querystring";
 
-dotenv.config();
+async function mainMethod() {
+  dotenv.config();
 
-const client = new Discord.Client();
-
-await client.login(process.env.BOT_TOKEN);
-
-const broadcastChannel = await client.channels.fetch('851286410237313028');
-if (!broadcastChannel.isText()) {
-  throw "Bound channel was not a text channel.";
-}
-
-const menteeWss = new Websocket.Server({ noServer: true });
-
-function parseHelpRequest(req) {
-  const url = new URL(req.url, 'http://example.com');
-
-  const user = {
-    name: url.searchParams.get('name'),
-    session: url.searchParams.get('session'),
-    language: url.searchParams.get('language'),
-    description: url.searchParams.get('description'),
+  const client = new Discord.Client();
+  
+  await client.login(process.env.BOT_TOKEN);
+  
+  const broadcastChannel = await client.channels.fetch(process.env.BOT_CHANNEL);
+  if (!broadcastChannel.isText()) {
+    throw "Bound channel was not a text channel.";
   }
-
-  return user;
-}
-
-menteeWss.on('connection', function newConnection(ws, _, req) {
-  const user = parseHelpRequest(req);
-
-  ws.on('message', async function GotMessage(msg) {
+  
+  const menteeWss = new Websocket.Server({ noServer: true });
+  
+  menteeWss.on('connection', async function newConnection(ws, req) {
+    const url = new URL(req.url, 'http://example.com');
+    const decoded_user = qs.decode(url.search.substr(1));
+    const user = {
+      name: decoded_user.name,
+      session: decoded_user.session,
+      language: decoded_user.language,
+      description: decoded_user.description,
+    }
+  
+    if (!user.name || !user.session || !user.language || !user.description) {
+      ws.close();
+      return;
+    }
+  
+    ws.on('message', async function GotMessage(msg) {
+      const request = qs.decode(msg.toString());
+    });
+  
     const embed = new Discord.MessageEmbed()
     .setTitle(`Assistance requested`)
     .addFields(
@@ -42,31 +46,33 @@ menteeWss.on('connection', function newConnection(ws, _, req) {
       { name: "Session", value: user.session },
     )
     .setTimestamp();
-
+  
     const sent = await broadcastChannel.send(embed);
     await sent.react('✅');
     const reactions = await sent.awaitReactions((reaction, user) => {
       return reaction.emoji.name === '✅' && user.id !== sent.author.id;
     }, { max: 1, time: 60000, errors: ["time"] });
     const reacterId = reactions.first().users.cache.find(user => !user.bot);
-
+  
     const requestClaimed = new Discord.MessageEmbed(embed)
       .setTitle(`Mentor On Site`)
       .addField('Responding Mentor', `${reacterId}`);
     await sent.edit(requestClaimed);
   });
-});
+  
+  const server = http.createServer();
+  server.on('upgrade', function upgrade(request, socket, head) {
+    const url = new URL(request.url, 'http://example.com');
+    if (url.pathname == '/mentee') {
+      menteeWss.handleUpgrade(request, socket, head, function done(ws) {
+        menteeWss.emit('connection', ws, request);
+      })
+    } else {
+      socket.destroy();
+    }
+  });
+  
+  server.listen(process.env.PORT);
+}
 
-const server = http.createServer();
-server.on('upgrade', function upgrade(request, socket, head) {
-  const url = new URL(request.url, 'http://example.com');
-  if (url.pathname == '/mentee') {
-    menteeWss.handleUpgrade(request, socket, head, function done(ws) {
-      menteeWss.emit('connection', ws, socket, request);
-    })
-  } else {
-    socket.destroy();
-  }
-});
-
-server.listen(8080);
+mainMethod();
