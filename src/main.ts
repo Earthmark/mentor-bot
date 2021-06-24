930868fb3661b756c5c2dbb594da211660afbc32
Daniel Miller (Earthmark) <earthmark.miller@gmail.com>
2021-06-24 03:02:22 -0700

    Added the ticket abstraction for review.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
 
+import { createDiscordStore, Ticket } from "./ticket";
 import { SubscriptionNotifier } from "./subs";
 import { logProm } from "./prom_catch";
 
 dotenv.config();
 
 logProm("Failure during startup")(async () => {
-  const notifier = new SubscriptionNotifier();
+  // We first bind to the discord channel, if that fails there's no reason to continue (we don't have a DB).
+  const store = await createDiscordStore(process.env.BOT_TOKEN ?? "");
+  // The notifier bridges the websocket server and maintainer, sending notifications of updates.
+  const notifier = new SubscriptionNotifier<Ticket>();
 
   console.log("Startup successful.");
 });
