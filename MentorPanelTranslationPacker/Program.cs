ecb96a13678724f61d11cf6d2d873cd8ca43ea92
Daniel Miller (Earthmark) <earthmark.miller@gmail.com>
2021-07-28 20:49:27 -0700

    Added some translation utilites.

---
+++ MentorPanelTranslationPacker/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace MentorPanelTranslationPacker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      using var file = File.CreateText("encodedLang.urlencoded");
+      foreach(var jsonFile in Directory.EnumerateFiles("./Locales", "*.json"))
+      {
+        var fileInfo = new FileInfo(jsonFile);
+        var messages = JObject.Parse(File.ReadAllText(jsonFile))["messages"] as JObject;
+        if (messages != null)
+        {
+          var message = MentorBot.UrlEncoder.Encode(messages);
+          file.WriteLine($"{fileInfo.Name}:{message}");
+        }
+      }
+    }
+  }
+}
